d49808005a1ad7fd6fba1228ac2418b392400e2d
Humberto <guayo@GuayoLap.(none)>
2011-02-22 17:36:29 -0600

    actualizacion de layout

--- proyecto/juez/public/javascripts/menu.js
+++ proyecto/juez/public/javascripts/menu.js
@@ -1,39 +1,5 @@
-$(function(){
-	$('#a a')
-		.css( {backgroundPosition: "-20px 35px"} )
-		.mouseover(function(){
-			$(this).stop().animate({backgroundPosition:"(-20px 94px)"}, {duration:500})
-		})
-		.mouseout(function(){
-			$(this).stop().animate({backgroundPosition:"(40px 35px)"}, {duration:200, complete:function(){
-				$(this).css({backgroundPosition: "-20px 35px"})
-			}})
-		})
-	$('#b a')
-		.css( {backgroundPosition: "0 0"} )
-		.mouseover(function(){
-			$(this).stop().animate({backgroundPosition:"(-150px 0)"}, {duration:500})
-		})
-		.mouseout(function(){
-			$(this).stop().animate({backgroundPosition:"(-300px 0)"}, {duration:200, complete:function(){
-				$(this).css({backgroundPosition: "0 0"})
-			}})
-		})
-	$('#c a')
-		.css( {backgroundPosition: "0 0"} )
-		.mouseover(function(){
-			$(this).stop().animate({backgroundPosition:"(0 -250px)"}, {duration:500})
-		})
-		.mouseout(function(){
-			$(this).stop().animate({backgroundPosition:"(0 0)"}, {duration:500})
-		})
-	$('#d a')
-		.css( {backgroundPosition: "0 0"} )
-		.mouseover(function(){
-			$(this).stop().animate({backgroundPosition:"(0 -250px)"}, {duration:500})
-		})
-		.mouseout(function(){
-			$(this).stop().animate({backgroundPosition:"(0 0)"}, {duration:500})
-		})
+$(document).ready(function(){
+  $("td.boton").click(function(){
+    $(this).animate({margin-left:"30px"},500);
+  });
 });
-
