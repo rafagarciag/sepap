d83c5406ce759dc5e071208f146bd4e64ecaca2a
rafa <rafagarciag90@gmail.com>
2011-02-22 20:12:21 -0600

    usuarios chidos en juez alternativo

--- proyecto/juez_prueba/config/routes.rb
+++ proyecto/juez_prueba/config/routes.rb
@@ -1,4 +1,9 @@
 JuezPrueba::Application.routes.draw do
+  devise_for :users
+
+  get "home/index"
+  root :to => "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
