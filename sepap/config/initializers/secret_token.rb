d633b3627c2f98b4e63fd55b407f64d627f6e1c1
Guayo <higgg.1989@gmail.com>
2011-02-27 18:06:47 -0600

    Penes Pelucos

---
+++ sepap/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sepap::Application.config.secret_token = '07ecb726ebea25e3d6e0dfddc1e099fa3204b281ef250884a0724dcee94a99141ad817302e6dd6de3c302149efa6d335c06d7b0a101de08abad2db166d66f200'
