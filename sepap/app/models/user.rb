5e8b04716080637c9d89066ca780ba80eeff2eed
rafa <rafagarciag90@gmail.com>
2011-02-27 19:45:21 -0600

    actualizacion de chariz

--- sepap/app/models/user.rb
+++ sepap/app/models/user.rb
@@ -5,5 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :nombre, :apellido
+
+  validates_presence_of :nombre, :apellidos
+
+  has_many :problems
 end
