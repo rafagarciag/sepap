4aa3b99a35b23b5159d315f1903c7e974b29b1bc
rafa <rafagarciag90@gmail.com>
2011-03-06 20:44:11 -0600

    ya jala el compilador ahora si

--- sepap/app/controllers/attempts_controller.rb
+++ sepap/app/controllers/attempts_controller.rb
@@ -52,6 +52,7 @@ class AttemptsController < ApplicationController
     # Aqui compila el codigo fuente y produce un resultado
     #incluir un if para cambiar la extension .java cuando se implemente otro lenguaje 
     archivo = "public/alumno/#{@attempt.user.matricula}/#{@attempt.numero_problema}/Problema#{@attempt.numero_problema}.java"
+    ejecutable = "public/alumno/#{@attempt.user.matricula}/#{@attempt.numero_problema} Problema#{@attempt.numero_problema}"
     entrada = "public/maestro/#{@attempt.numero_problema}/entrada"
     salida = "public/alumno/#{@attempt.user.matricula}/#{@attempt.numero_problema}/salida"
     salida_esperada = "public/maestro/#{@attempt.numero_problema}/salida_esperada"
@@ -59,7 +60,8 @@ class AttemptsController < ApplicationController
     
     #se llama al compilador
     #el formato del script es: compilarJava [archivo con el codigo del alumno] [entrada brindada por el profesor] [archivo donde se guarda la salida de ejecutar el archivo del alumno con las entradas del profesor] [salida esperada brindada por el profesor] [archivo donde se guardara la info de error en caso de no compilar]
-    @attempt.resultado = `./compilarJava2 #{archivo} #{entrada} #{salida} #{salida_esperada} #{error}`
+    @attempt.resultado = `./compilarJava2 #{archivo} '#{ejecutable}' #{entrada} #{salida} #{salida_esperada} #{error}`
+    #@attempt.resultado = `./compilarJava2 public/alumno/a01088197/1001/Problema1001.java public/maestro/1001/entrada public/alumno/a01088197/1001/salida public/maestro/1001/salida_esperada public/alumno/a01088197/1001/error` 
     
     #=========================================================
 
