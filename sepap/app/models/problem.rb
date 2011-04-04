7a3d0b7e7ce817681ca3a1869de01deedac69e2d
alan100000 <omgitsalan@gmail.com>
2011-04-03 19:30:11 -0500

    Aumentado tamaño de area de texto en forma de problemas

--- sepap/app/models/problem.rb
+++ sepap/app/models/problem.rb
@@ -11,15 +11,10 @@ class Problem < ActiveRecord::Base
 	validates_presence_of :titulo, :message => "Falta especificar el título del problema"
 	validates_presence_of :descripcion, :message => "Falta redactar la especificación del problema"
 	validates_uniqueness_of :numero, :message => "El número está duplicado"
-<<<<<<< HEAD
-	validates_presence_of :solution, :on=>:create
-	validates_presence_of :input, :on=>:create
-	validates_presence_of :output, :on=>:create
-=======
-	validates_presence_of :solution, :message => "Falta archivo de solución"
-	validates_presence_of :input, :message => "Falta archivo 'entrada'"
-	validates_presence_of :output, :message => "Falta archivo 'salida_esperada'"
->>>>>>> e8bfd771a2b36ea8b7bc0525866bf2a4c58531e7
+
+	validates_presence_of :solution, :on=>:create, :message => "Falta archivo de solución"
+	validates_presence_of :input, :on=>:create, :message => "Falta archivo 'entrada'"
+	validates_presence_of :output, :on=>:create, :message => "Falta archivo 'salida_esperada'"
 	
 	#Esto utiliza la gema carrierwave
 	mount_uploader :solution, SolutionUploader
