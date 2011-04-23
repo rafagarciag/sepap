8f25e07dc7b6831e9b776222e251df6ea400fb4f
rafa <rafagarciag90@gmail.com>
2011-04-23 03:52:19 -0500

    hice muchas cosas

---
+++ sepap/db/migrate/20110423064826_add_lenguaje_to_attempts.rb
@@ -0,0 +1,9 @@
+class AddLenguajeToAttempts < ActiveRecord::Migration
+  def self.up
+    add_column :attempts, :lenguaje, :string
+  end
+
+  def self.down
+    remove_column :attempts, :lenguaje
+  end
+end
