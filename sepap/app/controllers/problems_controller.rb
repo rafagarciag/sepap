726efe8b5c2841a515bcfddf657e14ccd964b629
Guayo <higgg.1989@gmail.com>
2011-04-27 10:59:21 -0500

    1

--- sepap/app/controllers/problems_controller.rb
+++ sepap/app/controllers/problems_controller.rb
@@ -73,6 +73,7 @@ class ProblemsController < ApplicationController
   # DELETE /problems/1
   # DELETE /problems/1.xml
   def destroy
+  	authorize! if can? :destroy, @problem
     @problem = Problem.find(params[:id])
     @problem.destroy
 
