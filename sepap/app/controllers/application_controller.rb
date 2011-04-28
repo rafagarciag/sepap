1fe6c59e120f51fa9b60844d88a550f1f3402582
Guayo <higgg.1989@gmail.com>
2011-04-28 17:17:25 -0500

    Ya quedo y asi

--- sepap/app/controllers/application_controller.rb
+++ sepap/app/controllers/application_controller.rb
@@ -2,5 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   rescue_from CanCan::AccessDenied do |exception|
   	flash[:error] = "Acceso DENEGADO"
+  	redirect_to root_url
   end
 end
