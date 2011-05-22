508782902263d5d865d1213c06f10fbc0afca2ce
Humberto <guayo@Batman.(none)>
2011-05-22 16:29:27 -0500

    Nuevo look :P

--- sepap/public/javascripts/application.js
+++ sepap/public/javascripts/application.js
@@ -1,2 +1,19 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+/*ANIMACION DEL MENU*/
+$(document).ready(function(){
+  $("a.botones").mouseover(function(){
+  	$(this).stop();
+  	$(this).css("border-color","#FFFFFF");
+  	$(this).animate({width:"100%"},"fast",function(){
+  		$(this).animate({backgroundPosition:'-400px 0px'},"fast");
+  	});
+  });    
+  $("a.botones").mouseleave(function(){
+  	$(this).stop();
+    $(this).animate({backgroundPosition: '0px 0px'},"fast",function(){
+    	$(this).animate({width:"50%"},"fast");
+    });
+  });
+});
--- sepap/public/javascripts/loading.js
+++ sepap/public/javascripts/loading.js
@@ -2,5 +2,4 @@ function clickBoton()
 {
 document.getElementById("attempt_submit").style.visibility="hidden";
 document.getElementById("loading").style.visibility="visible";
-
 }
