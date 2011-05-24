c5fd8a74ac86a6002d244d21ba822d56b995a93c
Roberto <roberto@Pin.(none)>
2011-05-24 17:02:43 -0500

    arregla jquery}

--- sepap/public/javascripts/application.js
+++ sepap/public/javascripts/application.js
@@ -1,19 +1,19 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-
+var $j = jQuery.noConflict();
 /*ANIMACION DEL MENU*/
-$(document).ready(function(){
-  $("a.botones").mouseover(function(){
-  	$(this).stop();
-  	$(this).css("border-color","#FFFFFF");
-  	$(this).animate({width:"100%"},"fast",function(){
-  		$(this).animate({backgroundPosition:'-400px 0px'},"fast");
+$j(document).ready(function(){
+  $j("a.botones").mouseover(function(){
+  	$j(this).stop();
+  	$j(this).css("border-color","#FFFFFF");
+  	$j(this).animate({width:"100%"},"fast",function(){
+  		$j(this).animate({backgroundPosition:'-400px 0px'},"fast");
   	});
   });    
-  $("a.botones").mouseleave(function(){
-  	$(this).stop();
-    $(this).animate({backgroundPosition: '0px 0px'},"fast",function(){
-    	$(this).animate({width:"50%"},"fast");
+  $j("a.botones").mouseleave(function(){
+  	$j(this).stop();
+    $j(this).animate({backgroundPosition: '0px 0px'},"fast",function(){
+    	$j(this).animate({width:"50%"},"fast");
     });
   });
 });
