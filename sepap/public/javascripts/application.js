8caa55e174d2925b47fba8c58d34749b25b4d855
Roberto <roberto@Pin.(none)>
2011-05-29 23:57:32 -0500

    agrega css

--- sepap/public/javascripts/application.js
+++ sepap/public/javascripts/application.js
@@ -2,18 +2,4 @@
 // This file is automatically included by javascript_include_tag :defaults
 var $j = jQuery.noConflict();
 /*ANIMACION DEL MENU*/
-$j(document).ready(function(){
-  $j("a.botones").mouseover(function(){
-  	$j(this).stop();
-  	$j(this).css("border-color","#FFFFFF");
-  	$j(this).animate({width:"100%"},"fast",function(){
-  		$j(this).animate({backgroundPosition:'-400px 0px'},"fast");
-  	});
-  });    
-  $j("a.botones").mouseleave(function(){
-  	$j(this).stop();
-    $j(this).animate({backgroundPosition: '0px 0px'},"fast",function(){
-    	$j(this).animate({width:"50%"},"fast");
-    });
-  });
-});
+
