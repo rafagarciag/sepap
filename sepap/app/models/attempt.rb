e6513bc3ebe27557e454be63b16d394d3fb2682e
Humberto <guayo@Batman.(none)>
2011-09-29 21:09:49 -0500

    pegar codigo

--- sepap/app/models/attempt.rb
+++ sepap/app/models/attempt.rb
@@ -2,18 +2,28 @@
 require 'file_size_validator'
 
 class Attempt < ActiveRecord::Base
-	attr_accessible :code, :numero_problema, :resultado, :lenguaje
+	attr_accessible :code, :numero_problema, :resultado, :lenguaje, :envio
+	attr_accessor :envio
 	
 	#Relaciones con otras clases
 	belongs_to :user
 	belongs_to :problem
 	
 	#Validaciones para la forma
-	validates_presence_of :code, :message => "Es necesario adjuntar un archivo con su código"
+	validates_presence_of :code, :message => "Es necesario adjuntar un archivo con su código", :unless=>:ignore_validation
 	validates_presence_of :problem_id, :message => "El número de problema no existe"
 	validates_numericality_of :numero_problema, :message => "El número de problema debe contener solamente números"
 
 	#esto utiliza la gema carrierwave 
 	mount_uploader :code, CodeUploader
 	validates :code,    :file_size => { :maximum => 0.1.megabytes.to_i }
+
+	def ignore_validation
+		if self.envio == "pegar"
+			true
+		else
+			false
+		end
+	end
 end
+
